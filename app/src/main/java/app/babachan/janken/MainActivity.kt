e2d66c65930f28afa15725cfd0b816405894e16d
lifeistech <[64596612+giraffecandy@users.noreply.github.com]>
2020-09-20 02:20:48 +0900

    ボタンを配列に入れた

--- app/src/main/java/app/babachan/janken/MainActivity.kt
+++ app/src/main/java/app/babachan/janken/MainActivity.kt
@@ -1,6 +1,7 @@
 package app.babachan.janken
 
-import android.graphics.Color
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,78 +11,100 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        gooButton.setOnClickListener {
-            player.text = "あなたの手はグーです"
-            val number = (0..2).random()
+        listOf(gooButton, paaButton, chokiButton).forEachIndexed { index, button ->
+            button.setOnClickListener {
+                val number = (0..2).random()
+                val data: SharedPreferences = getSharedPreferences("Data", Context.MODE_PRIVATE)
+                val editor = data.edit()
 
-            when (number){
-                0 -> {cpu.setImageResource(R.drawable.goo)
-                    result.text = "引き分けです"
-                    result.setTextColor(Color.parseColor("#8a000000"))
-                }
+                when (number) {
+                    0 -> {
+                        cpu.setImageResource(R.drawable.goo)
+                        editor.putString("Draw", "引き分け")
+                    }
+                    1 -> {
+                        cpu.setImageResource(R.drawable.choki)
+                        editor.putString("Win", "勝ち")
 
-                1-> {
-                    cpu.setImageResource(R.drawable.choki)
-                    result.text = "あなたの勝ちです"
-                    result.setTextColor(Color.RED)
-                }
+                    }
+                    2 -> {
+                        cpu.setImageResource(R.drawable.paa)
+                        editor.putString("lose", "負け")
+                    }
 
-                2 -> {
-                    cpu.setImageResource(R.drawable.paa)
-                    result.text = "あなたの負けです"
-                    result.setTextColor(Color.parseColor("#2196f3"))
-                }
-            }
-        }
-
-        chokiButton.setOnClickListener {
-            player.text = "あなたの手はチョキです"
-            val number: Int = (0..2).random()
-
-            when (number){
-                0 -> {cpu.setImageResource(R.drawable.choki)
-                    result.text = "引き分けです"
-                    result.setTextColor(Color.parseColor("#8a000000"))
-                }
-
-                1-> {
-                    cpu.setImageResource(R.drawable.paa)
-                    result.text = "あなたの勝ちです"
-                    result.setTextColor(Color.RED)
-                }
-
-                2 -> {
-                    cpu.setImageResource(R.drawable.goo)
-                    result.text = "あなたの負けです"
-                    result.setTextColor(Color.parseColor("#2196f3"))
                 }
             }
-
-        }
-
-        paaButton.setOnClickListener {
-            player.text = "あなたの手はパーです"
-            val number: Int = (0..2).random()
-
-            when (number){
-                0 -> {cpu.setImageResource(R.drawable.paa)
-                    result.text = "引き分けです"
-                    result.setTextColor(Color.parseColor("#8a000000"))
-                }
-
-                1-> {
-                    cpu.setImageResource(R.drawable.goo)
-                    result.text = "あなたの勝ちです"
-                    result.setTextColor(Color.RED)
-                }
-
-                2 -> {
-                    cpu.setImageResource(R.drawable.choki)
-                    result.text = "あなたの負けです"
-                    result.setTextColor(Color.parseColor("#2196f3"))
-                }
-            }
-
         }
     }
 }
+
+//gooButton.setOnClickListener {
+////            player.text = "あなたの手はグーです"
+//
+////                    result.text = "引き分けです"
+////                    result.setTextColor(Color.parseColor("#8a000000"))
+//
+//
+////                    result.text = "あなたの勝ちです"
+////                    result.setTextColor(Color.RED)
+//}
+//
+//cpu.setImageResource(R.drawable.paa)
+////                    result.text = "あなたの負けです"
+////                    result.setTextColor(Color.parseColor("#2196f3"))
+//}
+//}
+//}
+//
+//chokiButton.setOnClickListener {
+//    player.text = "あなたの手はチョキです"
+//    val number: Int = (0..2).random()
+//
+//    when (number) {
+//        0 -> {
+//            cpu.setImageResource(R.drawable.choki)
+//            result.text = "引き分けです"
+//            result.setTextColor(Color.parseColor("#8a000000"))
+//        }
+//
+//        1 -> {
+//            cpu.setImageResource(R.drawable.paa)
+//            result.text = "あなたの勝ちです"
+//            result.setTextColor(Color.RED)
+//        }
+//
+//        2 -> {
+//            cpu.setImageResource(R.drawable.goo)
+//            result.text = "あなたの負けです"
+//            result.setTextColor(Color.parseColor("#2196f3"))
+//        }
+//    }
+//
+//}
+//
+//paaButton.setOnClickListener {
+//    player.text = "あなたの手はパーです"
+//    val number: Int = (0..2).random()
+//
+//    when (number) {
+//        0 -> {
+//            cpu.setImageResource(R.drawable.paa)
+//            result.text = "引き分けです"
+//            result.setTextColor(Color.parseColor("#8a000000"))
+//        }
+//
+//        1 -> {
+//            cpu.setImageResource(R.drawable.goo)
+//            result.text = "あなたの勝ちです"
+//            result.setTextColor(Color.RED)
+//        }
+//
+//        2 -> {
+//            cpu.setImageResource(R.drawable.choki)
+//            result.text = "あなたの負けです"
+//            result.setTextColor(Color.parseColor("#2196f3"))
+//        }
+//    }
+//
+//}
+
